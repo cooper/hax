a6c2561b1c0db2e448fb4d64296ee389bc8db4cb
Mitchell Cooper <cooper879@msn.com>
2013-05-15 21:22:27 -0400

    get_symbol() and get_symbol_ref()

--- Hax/Package.pm
+++ Hax/Package.pm
@@ -3,6 +3,7 @@ package Hax::Package;
 
 use warnings;
 use strict;
+use feature 'switch';
 
 use Hax::Exporter prefix => 'package';
 
@@ -11,7 +12,7 @@ use Hax::Exporter prefix => 'package';
 # package_make_child_of('Evented::Person', 'EventedObject', 1)
 sub make_child_of {
     my ($package, $make_parent, $at_end) = @_;
-    my $isa = get_variable($package, '@ISA');
+    my $isa = get_symbol($package, '@ISA');
     
     # package already inherits directly.
     return 1 if $make_parent ~~ @$isa;
@@ -29,10 +30,33 @@ sub make_child_of {
     
 }
 
-sub get_variable {
+# fetch a symbol from package's symbol table.
+sub get_symbol {
+    my ($package, $variable, $ref) = @_;
+    
+    # must start with a sigil.
+    return if $variable !~ m/^([@\*%\$])(\w+)$/;
+    my ($sigil, $var_name) = ($1, $2);
+    
+    my $symbol = $package.q(::).$var_name;
+    no strict 'refs';
+ 
+    # find the symbol.
+    given ($sigil) {
+        when ('$') { return $ref ? \$$symbol : $$symbol }
+        when ('@') { return $ref ? \@$symbol : @$symbol }
+        when ('*') { return $ref ? \*$symbol : *$symbol }
+        when ('%') { return $ref ? \&$symbol : %$symbol }
+    }
+    
+    return;
 }
 
-sub set_variable {
+# fetch a reference to a symbol.
+sub get_symbol_ref { get_symbol(@_, 1) }
+
+# set a symbol in package's symbol table.
+sub set_symbol {
 }
 
 # borrow Hax::Exporter's code exporter.
